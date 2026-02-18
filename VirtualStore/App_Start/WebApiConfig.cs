ee26ecca5ec358b035330c91b077538fbfadd577
Randy <rndymi@no-replay.com>
2026-02-19 00:49:52 +0100

    feat: add Web Api

--- VirtualStore/App_Start/WebApiConfig.cs
+++ VirtualStore/App_Start/WebApiConfig.cs
@@ -14,6 +14,8 @@ namespace VirtualStore
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ VirtualStore/Controllers/ProductsAPIController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VirtualStore.Models;
+
+namespace VirtualStore.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductsAPIController : ApiController
+    {
+
+        private ModelVirtualStoreContainer con = new ModelVirtualStoreContainer();
+
+        // GET: api/products
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            var products = con.Products
+                .Where(p => p.isActiveProd)
+                .Select(p => new
+                {
+                    p.Id,
+                    name = p.nameProd,
+                    description = p.descripProd,
+                    p.price,
+                    stock = p.stockProd,
+                    image = p.imageProd
+                })
+                .ToList();
+
+            return Ok(products);
+        }
+    }
+}
